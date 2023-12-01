55b41d733dd10f542b4702ff500368d2fed073b2
Jalquicira <75825276+Jalquicira@users.noreply.github.com>
2023-11-30 19:18:26 -0800

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 function add(number1, number2) {
-    return number1 + number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result = add(number1, number2);
-  alert(result);
+  return number1 + number2;
+};
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+
+$(document).ready(function() {
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    var operator = document.getElementById("#expression");
+    var result = operator.option[operator.selectedIndex].value;
+      $("#output").text(result);
+    });
+});
